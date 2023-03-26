b39599053a06eef5b430ebe47e38f928966cfd1c
Manan Kansara <mkansara0903@gmail.com>
2023-03-26 18:13:45 +0000

    Project Done

--- README.md
+++ README.md
@@ -1,10 +1,9 @@
-# Node.js template
+# 🌟 Magic of Asynchronous in Nodejs 🌟   
 
-This is a Node.js project.
+## Kepler Data processing.
 
-Add your [configuration](https://codesandbox.io/docs/projects/learn/setting-up/tasks) to optimize it for [CodeSandbox](https://codesandbox.io/p/dashboard).
-
-## Resources
-
-- [CodeSandbox — Docs](https://codesandbox.io/docs/projects)
-- [CodeSandbox — Discord](https://discord.gg/Ggarp3pX5H)
+- Here We used CSV data to processing All data that look like similar to Earth data.
+- Here in this project we use nodejs inbuilt fs module to processed data.
+- We use createReadStream() function which return data as buffer stream and that buffer stream pipe to another writeable stream as parse() function which we use library.
+- Then all processed data is filler with isHabitablePlanets() function.
+- Here in this project out off 8000-10000 data there is fillter out only 8 planet which is related to earth and thier possibilties to life at that planet.
--- index.js
+++ index.js
@@ -1,1 +1,43 @@
+// We can read file using inbuilt node file system module.we use fs module
+const fs = require('fs')
+//but this file system module has no idea of csv file.
+//when we use text file as input they red as string file.
+//here we using csv-parse module.
+const { parse } = require('csv-parse')
+const resault = [];
+//Here making function that checking that planet is habitable or not ?
+function isHabitablePlanets(planet) {
+    t1 = Date.now()
+    return planet['koi_disposition'] === 'CONFIRMED' && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 && planet['koi_prad'] < 1.6;
+}
+//This method return bit of stream as buffer object.
+const file = fs.createReadStream('Data.csv')
+    //here pipe function is used to connect two stream like readbale and writeable text.
+    //Link : https://www.educative.io/answers/what-is-stream-module-pipe-in-nodejs
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+    }))
+    .on('data', (data) => {
+        if (isHabitablePlanets(data)) {
+            resault.push(data)
+        }
+
+
+    })
+    .on('error', (err) => {
+        console.log(`There is error : ${err.message}`)
+    })
+    .on('end', () => {
+        console.log(`Total habitable planet is ${resault.length} and resault are : `)
+        // for (i = 0; i < resault.length; i++) {
+        //     console.log(resault[i].kepoi_name);
+        // }
+        //map through array
+        resault.map((planet)=>{console.log(planet.kepoi_name)})
+        t2 = Date.now();
+        console.log(`Your Date Done processing data in ${Math.floor((t2-t1))}ms`)
+        console.log("Done processing data.")
+    });
+
 
